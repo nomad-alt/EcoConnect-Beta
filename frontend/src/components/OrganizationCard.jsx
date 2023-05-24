dca93a466f34495248319a56ef2706f6523623c2
majid0334 <113986626+majid0334@users.noreply.github.com>
2023-05-24 12:37:19 +0200

    Added share button

--- frontend/src/components/OrganizationCard.jsx
+++ frontend/src/components/OrganizationCard.jsx
@@ -1,46 +1,61 @@
-import React from 'react';
+import React from "react";
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+} from "react-share";
 
 const OrganizationCard = ({ organization }) => {
-  const { name, description, category, website, donateLink, additionalLinks, imageUrl } = organization;
+  const shareUrl = window.location.href;
+  const {
+    name,
+    description,
+    category,
+    website,
+    donateLink,
+    additionalLinks,
+    imageUrl,
+  } = organization;
 
- const handleLike = async () => {
-  if (!user) {
-    history.push('/login');
-    return;
-  }
-
-  try {
-    const response = await fetch(
-      `/api/users/${user.id}/likedOrganizations/${organization.id}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${user.token}`,
-        },
-      }
-    );
-
-    if (response.ok) {
-      // Update the state of your component to reflect that the organization has been liked
-      // This is just an example. Your actual state update logic might be different.
-      setLiked(true);
-    } else {
-      // Handle any non-200 HTTP status codes
-      console.error('An error occurred while liking the organization');
+  const handleLike = async () => {
+    if (!user) {
+      history.push("/login");
+      return;
     }
-  } catch (error) {
-    // Handle any errors that occur while sending the request
-    console.error('An error occurred while liking the organization', error);
-  }
-};
+
+    try {
+      const response = await fetch(
+        `/api/users/${user.id}/likedOrganizations/${organization.id}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+
+      if (response.ok) {
+        // Update the state of your component to reflect that the organization has been liked
+        // This is just an example. Your actual state update logic might be different.
+        setLiked(true);
+      } else {
+        // Handle any non-200 HTTP status codes
+        console.error("An error occurred while liking the organization");
+      }
+    } catch (error) {
+      // Handle any errors that occur while sending the request
+      console.error("An error occurred while liking the organization", error);
+    }
+  };
 
   const handleShare = () => {
     // the share functionality here
   };
 
   const handleDonate = () => {
-    window.open(donateLink, '_blank');
+    window.open(donateLink, "_blank");
   };
 
   return (
@@ -49,13 +64,59 @@ const OrganizationCard = ({ organization }) => {
       <h3 className="organization-name">{name}</h3>
       <p className="organization-description">{description}</p>
       <p className="organization-category">{category}</p>
-      <a href={website} target="_blank" rel="noopener noreferrer" className="organization-website">
+      <a
+        href={website}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="organization-website"
+      >
         Visit Website
       </a>
       <div className="organization-buttons">
-        <button onClick={handleLike} className="organization-like">Like</button>
-        <button onClick={handleShare} className="organization-share">Share</button>
-        <button onClick={handleDonate} className="organization-donate">Donate</button>
+        <button onClick={handleLike} className="organization-like">
+          Like
+        </button>
+        <button
+          /*    onClick={handleShare} */
+          onClick={() => {
+            navigator.clipboard.writeText(shareUrl);
+          }}
+        >
+          Copy to clickbord
+        </button>
+        <EmailShareButton
+          url={shareUrl}
+          separator
+          subject={name}
+          body={description}
+        >
+          Emai
+        </EmailShareButton>
+
+        <LinkedinShareButton url={shareUrl} className="organization-share">
+          Linkiden
+        </LinkedinShareButton>
+
+        <TwitterShareButton
+          url={shareUrl}
+          title={name}
+          hashtags={["chasAcademy", "EcoConnect"]}
+          className="organization-share"
+        >
+          Twiteer
+        </TwitterShareButton>
+        <FacebookShareButton
+          url={shareUrl}
+          quote={"Take care of envirement"}
+          description={description}
+          hashtags={["chasAcademy", "EcoConnect"]}
+        >
+          Facebook
+        </FacebookShareButton>
+
+        <button onClick={handleDonate} className="organization-donate">
+          Donate
+        </button>
       </div>
       <ul className="additional-links">
         {additionalLinks.map((link, index) => (
