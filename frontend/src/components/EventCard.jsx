a8a5be8b7fb99a5a187c7c93155218116f760412
majid0334 <113986626+majid0334@users.noreply.github.com>
2023-06-10 11:11:21 +0200

    added share button gor event

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+} from "react-share";
 
 const EventCard = ({ event, onInterest, onUninterest, interested }) => {
+  const shareUrl = window.location.href;
+  console.log(`This is the link for this page: ${shareUrl}`);
   return (
-  <div className="event-card">
-    <img src={event.imageUrl} alt={event.title} />
-    <h2>{event.title}</h2>
-    <p>{event.location}</p>
-    <p>{event.startDate}</p>
-    <button onClick={interested ? () => onUninterest(event._id) : () => onInterest(event._id)}>
-      {interested ? "Not Interested" : "Interested"}
-    </button>
-  </div>
-);
+    <div className="event-card">
+      <img src={event.imageUrl} alt={event.title} />
+      <h2>{event.title}</h2>
+      <p>{event.location}</p>
+      <p>{event.startDate}</p>
+      <LinkedinShareButton url={shareUrl} title={event.title}>
+        Share on LinkedIn
+      </LinkedinShareButton>
+
+      <TwitterShareButton
+        url={shareUrl}
+        title={event.title}
+        hashtags={["chasAcademy", "EcoConnect"]}
+        className="organization-share"
+      >
+        Twitter
+      </TwitterShareButton>
+
+      <FacebookShareButton
+        url={shareUrl}
+        quote={"Take care of environment"}
+        description={event.title}
+        hashtags={["chasAcademy", "EcoConnect"]}
+      >
+        Facebook
+      </FacebookShareButton>
+      <button
+        onClick={
+          interested
+            ? () => onUninterest(event._id)
+            : () => onInterest(event._id)
+        }
+      >
+        {interested ? "Not Interested" : "Interested"}
+      </button>
+    </div>
+  );
 };
 
 EventCard.propTypes = {
@@ -23,4 +57,3 @@ EventCard.propTypes = {
 };
 
 export default EventCard;
-
