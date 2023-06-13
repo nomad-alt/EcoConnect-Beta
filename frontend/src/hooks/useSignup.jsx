5f1c3718df07644f4d60bd2109c73bda08877008
nomad-alt <nomad-alt@proton.me>
2023-06-13 14:54:24 +0200

    fixed signup issue

--- frontend/src/hooks/useSignup.jsx
+++ frontend/src/hooks/useSignup.jsx
@@ -1,40 +1,44 @@
 import { useState } from 'react';
 import { useAuthContext } from './useAuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const useSignup = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
   const { dispatch } = useAuthContext();
+  const navigate = useNavigate();
 
   const signup = async (email, password) => {
-    setIsLoading(true);
-    setError(null);
+  setIsLoading(true);
+  setError(null);
 
-    const response = await fetch('/api/user/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password }),
-    });
+  const response = await fetch('/api/users/signup', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  });
 
-    const json = await response.json();
+  if (!response.ok) {
+    const json = await response.text();
+    setIsLoading(false);
+    setError(json && json.error ? json.error : 'Something went wrong');
+    return;
+  }
 
-if (!response.ok) {
+  const json = await response.json();
+  
+  // save the user to local storage
+  localStorage.setItem('user', JSON.stringify(json))
+
+  // update the auth context
+  dispatch({ type: 'LOGIN', payload: { ...json, _id: json._id } })
+
+   // navigate to the dashboard page
+  navigate('/dashboard');
+
+  // update loading state
   setIsLoading(false);
-  setError(json && json.error ? json.error : 'Something went wrong');
-  return;
-}
-
-    if (response.ok) {
-      // save the user to local storage
-      localStorage.setItem('user', JSON.stringify(json))
-
-      // update the auth context
-      dispatch({ type: 'LOGIN', payload: { ...json, _id: json._id } })
-
-      // update loading state
-      setIsLoading(false);
-    }
-  };
+};
 
   return { signup, isLoading, error };
 };
